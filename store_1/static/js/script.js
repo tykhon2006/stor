3ba7b9dc373e237e951bebff871cdd73c3e27971
tykhon2006 <tisha8690@gmail.com>
2022-08-21 21:25:18 +0200

    added css

--- store_1/static/js/script.js
+++ store_1/static/js/script.js
@@ -3,10 +3,8 @@ function show_active_link(menu){
         let elem = document.getElementById(menu).querySelectorAll("a")
         let url = document.location.href
         for(link in elem){
-            if (elem[link].href != url) {
-                console.log(elem[link].href)
-                console.log(url)
-                elem[link].classList.add("text-secondary")
+            if (elem[link].href == url) {
+                elem[link].classList.add("active")
             }
         }
     } catch{}
